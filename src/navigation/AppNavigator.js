97786e1049fe7a64065e960d90bc867c093fd655
muratcangebes <muratcan.gebes19@gmail.com>
2024-08-28 16:44:42 +0300

    Screen sayfaları eklendi (HomeScreen,ProfileScreen). AppNavigator.js içerisi düzenlenip screens sayfaları eklendi.

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,23 +2,10 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
-//Bottom Tabs denemek için yapıldı buraya sayfalar!
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
@@ -27,7 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
